cc88f5f0bd832e56d5d2030068e8b0a3cf24f0c6
chimay <remi.dore@protonmail.com>
2023-09-26 17:02:05 +0200

    license

--- objects/enum.py
+++ objects/enum.py
@@ -7,7 +7,6 @@
 # {{{ Enum
 
 class Enum(object):
-
 	def __init__(self, *args):
 		self.__dict__.update(zip(args, range(len(args))))
 
@@ -19,14 +18,11 @@ class Enum(object):
 # {{{ main
 
 if __name__ == '__main__' :
-
 	try :
 		import psyco
 		psyco.full()
 	except ImportError : pass
-
 	colors = Enum("Red", "Green", "Blue")
-
 	print colors.Red
 	print colors.Green
 	print colors.Blue
--- utils/iteratio.py
+++ utils/iteratio.py
@@ -38,7 +38,7 @@ def combinaison(*arguments) :
 
 if __name__ == '__main__' :
 	for e in combinaison(range(2), range(1, 4), range(2, 4), range(3, 5)) :
-		print e
+		print(e)
 	pass
 
 # }}}
